ef447c924509dbf8095639edd4af0f7e012aa589
Amritesh Sharma <Amritesh07071996@gmail.com>
2016-06-06 00:11:37 +0530

    q2 today

--- mainq2.m
+++ mainq2.m
@@ -12,42 +12,29 @@ elseif type == 'CH'
    b =  1E-9;   c =  -40E-3;   d =  150E-12;   Vpeak=25E-3;
 end
 
-
-
-
 N = 10; % No of neurons
-dt = 0.1E-3; % time step
+h = 0.1E-3; % time step
 Tmax = 0.5; %max time
-Iapp = 250E-12; % applied current
-M = Tmax/dt; % max time index
-
-%some constants describing the model
-gL = 30*10^-9 ;% mhos
-EL = -70*10^-3 ;% Volts
-C  = 300*10^-12 ;% Farad
-VT = 20*10^-3 ;% Volts
-
-% inputs
-M = 500/0.1 ; % no. of columns (no. of delta_T time intervals that amount to T)  
-N = 2 ; % no. of rows (no. of neurons) 
-T = 0.5 ; % the time upto which the expt is performed
-h = 0.0001 ; % delta_T
+M = Tmax/dt; % max time index i.e no. of columns (no. of delta_T time intervals that amount to T)
 
 % input current matrix
-Ic = gL*(VT-EL) ; %considering Ic = 1
-alpha = 0.1;
+    %Ic = gL*(VT-EL) ; %considering Ic = 1
+    %alpha = 0.1;
 I = zeros(N,M);
 for i = 1:N
-    I(i,1:M) = (1 + alpha*i)*Ic;
+    I(i,1:M) = 400*10^-12 ; %(1 + alpha*i)*Ic;
+    % I(i,1:M) = 500*10^-12 ;
+    % I(i,1:M) = 600*10^-12 ;
 end
 
-A = kz/C ; 
-B = gL*EL ;
-
 y = zeros(N,M);
+U = zeros(N,M);
 % init condition
-y(:,1) = EL; 
-F = @(t,V,U,I) A*(V-Et)*(V-Er)+(-U+I)/C ; 
+y(:,1) = Er;
+U(:,1) = 0;
+
+
+F = @(t,V,U,I) (Kz*(V-Et).*(V-Er)-U+I)/C ; 
 G = @(t,V,U) a*(b*(V-Er)-U) ; 
 
 %runge kutta implementation
@@ -75,27 +62,7 @@ for i=1:N
     plot(t,y(i,:))
     hold on
 end
+
 title('Spiking Neuron')
 xlabel('Time (in ms)')
 ylabel('Membrane Potential (in Volts)')
-
-%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
-t1 = find(y(2,4000:5000)==-0.07);
-t2 = find(y(4,4000:5000)==-0.07);
-t3 = find(y(6,4000:5000)==-0.07);
-t4 = find(y(8,4000:5000)==-0.07);
-
-avg_t1 = t1(2)-t1(1);
-avg_t2 = t2(2)-t2(1);
-avg_t3 = t3(2)-t3(1);
-avg_t4 = t4(2)-t4(1);
-
-avg_t = [avg_t1 avg_t2 avg_t3 avg_t4];
-Iappk = [I(2) I(4) I(6) I(8)];
-
-figure(2)
-plot(Iappk,avg_t)
-
-title('Avg time of spiking vs time')
-ylabel('Time (in ms)')
-xlabel('Iappk (in nA)')
